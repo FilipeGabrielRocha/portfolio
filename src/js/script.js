370513384a705ec7b9c2674918e5456fe2d8ebfd
FilipeGabrielRocha <filiperochaprogramador@gmail.com>
2024-02-05 20:18:05 -0300

    fazendo a section habilidades

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,25 @@
-alert('Status do Site: Construção')
+// alert('Status do Site: Construção')
+
+function getAge(){
+    let date = new Date()
+    let year = date.getFullYear()
+    let month = date.getMonth() + 1
+    let day = date.getDate()
+    const yearOfBirth = 2000
+
+    if (month === 11 && day >= 14){
+        let age = year - yearOfBirth
+        return age
+    } else {
+        let age = (year - yearOfBirth) - 1
+        return age
+    }
+
+}
+
+function showAge(){
+    let age = getAge()
+    document.getElementById('age').innerHTML = age
+}
+
+window.onload = showAge
