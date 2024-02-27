e3c0137ccf766be34315f1ed81afc50a4406ecbb
FilipeGabrielRocha <filiperochaprogramador@gmail.com>
2024-02-27 13:20:17 -0300

    voltando ao normal

--- src/js/teste.js
+++
@@ -1,7 +0,0 @@
-fetch(
-    "https://app.typebot.io/api/v1/typebots/clt4eojwf0004123lj2cspww4/results",
-    options
-  )
-    .then((response) => response.json())
-    .then((response) => console.log(response))
-    .catch((err) => console.error(err));
