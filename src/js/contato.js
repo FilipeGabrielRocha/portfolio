9d81a210be6676809d274e764700e11141d5144e
Filipe Gabriel Rocha <94459039+FilipeGabrielRocha@users.noreply.github.com>
2025-06-26 10:53:50 -0300

    página contato criado

---
+++ src/js/contato.js
@@ -0,0 +1,5 @@
+const btnEnviar = document.querySelector('#send');
+
+btnEnviar.addEventListener('click', (e) => {
+    alert('Obrigado pela sua mensagem! Entrarei em contato com você assim que possível.');
+})
